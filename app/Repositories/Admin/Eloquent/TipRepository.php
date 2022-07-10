ed27efac15ebb7390f49723496c165dd865d4a4f
user <user@pop-os.localdomain>
2022-07-10 17:34:41 -0300

    fix(tips): fixes interface implementation tips

--- app/Repositories/Admin/Eloquent/TipRepository.php
+++ app/Repositories/Admin/Eloquent/TipRepository.php
@@ -3,9 +3,9 @@
 namespace App\Repositories\Admin\Eloquent;
 
 use App\Models\Tip;
-use App\Repositories\Admin\Contracts\RecipeRepositoryInterface;
+use App\Repositories\Admin\Contracts\TipRepositoryInterface;
 
-class TipRepository extends AbstractRepository implements RecipeRepositoryInterface
+class TipRepository extends AbstractRepository implements TipRepositoryInterface
 {
     protected $model = Tip::class;
 }
